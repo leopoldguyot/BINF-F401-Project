d92a133078a9234c436bc5bf2fa8aeb1d23a6155
leopoldguyot <leopold.guyot13@gmail.com>
2024-04-16 15:06:14 +0200

    [UPD] - ggpairs for cor visualisation

--- clinical_data_exploration.R
+++ clinical_data_exploration.R
@@ -1,13 +1,14 @@
 ######### Packages Import #########
 library("ggplot2")
+library("GGally")
 
 ######### Functions Definition #########
 distribution_plot <- function(data, variable, title){
   if(is.numeric(data[[variable]])) {
-    plot <- ggplot(data, aes(x = data[[variable]])) +
+    plot <- ggplot(data, aes(x = .data[[variable]])) +
       geom_histogram(fill = "skyblue", color = "black")
   } else {
-    plot <- ggplot(data, aes(x = data[[variable]])) +
+    plot <- ggplot(data, aes(x = .data[[variable]])) +
       geom_bar(fill = "skyblue", color = "black")
   }
   
@@ -24,7 +25,7 @@ clinical_data$SEX <- as.factor(clinical_data$SEX)
 clinical_data$DTHVNT <- as.factor(clinical_data$DTHVNT)
 clinical_data$DTHHRDY <- as.factor(clinical_data$DTHHRDY)
 
-plot <- lapply(colnames(clinical_data)[-c(1:2, 13)], function(x){
+plot <- lapply(colnames(clinical_data)[-c(1:2, 13:14)], function(x){
     plot <- distribution_plot(clinical_data, x, x)
     path <- file.path("figures", "clinical_data_var_dist", paste0(x, ".pdf"))
     ggsave(filename = path,
@@ -32,3 +33,11 @@ plot <- lapply(colnames(clinical_data)[-c(1:2, 13)], function(x){
      width = 10,
       height = 10)
     })
+
+ggpairs_plot <- GGally::ggpairs(clinical_data[-c(1,2,13,14)])
+ggsave(filename = file.path("figures",
+                            "clinical_data_exploration",
+                            "ggpairs_plot.pdf"), 
+       ggpairs_plot,
+       width = 15,
+       height = 10)
